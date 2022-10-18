ad277cce305e971a893f6b209d976978eb6eccd6
“Olga <slesarenko.h@gmail.com>
2022-10-18 15:24:40 +0300

    Uncompiled file from metanit

---
+++ Program1/Program.java
@@ -0,0 +1,6 @@
+public class Program{
+  public static void main (String args[]){
+	  int salary = 100000000;
+  System.out.println("Hello Java. My future salary "+salary+"$"+", I'll do my best");
+  }
+}
